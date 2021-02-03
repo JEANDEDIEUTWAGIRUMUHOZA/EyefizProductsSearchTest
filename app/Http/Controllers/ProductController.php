8f304a3cdab3d4e07e9f425d0610b409db909376
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-03 23:25:17 +0100

    created index() function to display index.blade.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    //function to display the view index.blade.php
+    public function index()
+    {
+        return view('products.index');
+    }
+    
 }
