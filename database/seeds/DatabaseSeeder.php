545e5a9a5b16960ee03d6b4e050aa7f5676139d9
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-04 01:52:36 +0100

    call for ProductsTable seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //to populate our table after $faker fields creation
+        $this->call(ProductsTableSeeder::class);
     }
 }
