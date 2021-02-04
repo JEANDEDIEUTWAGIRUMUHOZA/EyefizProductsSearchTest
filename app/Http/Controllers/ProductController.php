2b19a4b5ad64f5b17707c4c23cae1dd9db7041c1
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-04 01:46:56 +0100

    generate and display products randomly and inject them to the view index.blade.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -9,7 +9,10 @@ class ProductController extends Controller
     //function to display the view index.blade.php
     public function index()
     {
-        return view('products.index');
+        //display randomly 6 products
+        $products = Product::inRandomOrder()->take(9)->get();
+        // dd($products);/test 6 products random display, then we inject them in our vue
+        return view('products.index')->with('products', $products);
     }
     
 }
