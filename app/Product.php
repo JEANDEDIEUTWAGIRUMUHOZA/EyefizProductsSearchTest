a282f3d21f1ccda65a92b005f831cfc6548bc2b4
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-04 03:12:21 +0100

    formatted getPrice() function

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //format product price
+    
+    public function getPrice(){
+        $price = $this->price / 100;
+
+        return number_format($price, 2, ',', ' ') . ' €';
+    }
 }
