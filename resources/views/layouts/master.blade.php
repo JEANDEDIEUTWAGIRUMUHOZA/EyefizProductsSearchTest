3176ae2ca860fd5a6d65b642d9d629caf9ef7fba
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-06 19:28:23 +0100

    corrected search form error in master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,7 +33,7 @@
         <form action="{{ route('products.search') }}" class="d-flex mr-1">
 
 <div class="form-group mb-0 mr-4">
-<input type="text" name="q" class="form-control" placeholder="produit ou categorie">
+<input type="text" name="q" class="form-control" placeholder="produit ou catégorie">
 </div>
 
 <button type="submit" class="btn btn-info">Chercher</button>
